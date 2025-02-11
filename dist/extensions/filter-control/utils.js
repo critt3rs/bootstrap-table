099164f406687365687ec2524b02c49a4f9aed3b
Ahmad Azwan <vondie@gmail.com>
2025-02-11 18:14:00 +0800

    Update utils.js
    only apply when elem is input or textarea

--- dist/extensions/filter-control/utils.js
+++ dist/extensions/filter-control/utils.js
@@ -3854,7 +3854,7 @@
           range.move('character', caretPos);
           range.select();
         } else {
-          elem.setSelectionRange(caretPos, caretPos);
+          if (['input', 'textarea'].indexOf(elem.tagName.toLowerCase()) !== -1) elem.setSelectionRange(caretPos, caretPos);
         }
       }
     } catch (ex) {
